9dbcf725001726ba0f9fafcf6864e62112b16ad0
abdullateefakorede <abdullateefademoye41@gmail.com>
2023-08-09 00:11:23 +0100

    chore: file upload path changed

--- constants/file-upload.js
+++ constants/file-upload.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 exports.upload = multer({
   storage: multer.diskStorage({
-    destination: './uploads/',
+    destination: 'uploads/',
     filename: (_req, file, cb) => {
-      return cb(null, Date.now() + file.originalname)
+      return cb(null, Date.now() + file.originalname.split(' ')[0])
     },
     limits: { fileSize: 4194304 },
     fileFilter: (_req, file, callback) => {
